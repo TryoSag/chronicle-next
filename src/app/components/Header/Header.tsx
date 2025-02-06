52d158e083bd78cef4a8a5ff9d8dd0ad1c8107b7
AlbSag <92945087+TryoSag@users.noreply.github.com>
2025-02-06 12:25:24 +0100

    refactor header img to image and src

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,11 +1,17 @@
 import { JSX } from "react";
+import Image from "next/image";
 
 const Header = (): JSX.Element => {
   return (
-    <header className="container-header">
-      <img src="../../../public/images/appIcon.png" alt="App logo" />
+    <header>
+      <Image
+        src="@/src/app/images/appIcon.png"
+        alt="App logo"
+        width={50}
+        height={50}
+      />
       <h1 className="text-title">Chronicler</h1>
-      <button className="icon-logout" />
+      <button className="icon-logout">Adios</button>
     </header>
   );
 };
