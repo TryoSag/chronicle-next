e71478a1b622cbe9e4edad668b1a9298fdd59aa8
AlbSag <92945087+TryoSag@users.noreply.github.com>
2025-02-06 11:54:34 +0100

    rename pages to navigate

---
+++ src/pages/chroniclesvv/page.tsx
@@ -0,0 +1,6 @@
+import ChronclesList from "@/src/app/components/ChroniclesList/ChroniclesList";
+import { JSX } from "react";
+
+const chronicles = (): JSX.Element => <ChronclesList />;
+
+export default chronicles;
