8cf456f1298a6174b4e978b40f2d295f517f4528
AlbSag <92945087+TryoSag@users.noreply.github.com>
2025-02-21 13:45:42 +0100

    start working with the login page on home

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
-export default function Home() {
+import Link from "next/link";
+import { JSX } from "react";
+
+export default function Home(): JSX.Element {
   return (
-   <div className="background">
-    PATATA
-   </div>
-  )
+    <div className="background">
+      <Link href="./login">{`Login -->`}</Link>
+    </div>
+  );
 }
