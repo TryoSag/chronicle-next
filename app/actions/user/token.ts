77b80c3fe348a2d64d938fb69a028788070df7dc
AlbSag <92945087+TryoSag@users.noreply.github.com>
2025-03-13 16:40:19 +0100

    refactor functions and add checkToken

--- app/actions/user/token.ts
+++ app/actions/user/token.ts
@@ -1,4 +1,6 @@
 "use server";
+import { tokenName } from "@/constants/components";
+import prisma from "@/lib/prisma";
 import { IToken } from "@/types/userTypes";
 import jwt from "jsonwebtoken";
 import { jwtDecode } from "jwt-decode";
@@ -7,19 +9,29 @@ export const generateToken = async ({
   name,
   id,
 }: IToken): Promise<{ userToken: string }> => {
-  if (!process.env.JWT_SECRET) {
-    throw new Error("JWT_SECRET not found in .env");
-  }
-  const userToken = await jwt.sign({ name, id }, process.env.JWT_SECRET);
+  const userToken = await jwt.sign({ name, id }, process.env.JWT_SECRET!);
 
   return { userToken };
 };
 
-export const openToken = async (token: string): Promise<IToken> => {
-  if (!process.env.JWT_SECRET) {
-    throw new Error("JWT_SECRET not found in .env");
+export const checkToken = async (token: string): Promise<boolean> => {
+  try {
+    await jwt.verify(token, process.env.JWT_SECRET!);
+    const { id } = await jwtDecode<IToken>(token);
+    const userOnDb = await prisma.user.findUnique({
+      where: { id },
+    });
+    if (userOnDb) return true;
+    return false;
+  } catch (error) {
+    console.error(error);
+    return false;
   }
-  const { name, id } = await jwtDecode<IToken>(token);
+};
+
+export const openToken = async (): Promise<IToken> => {
+  const token: string | null = await localStorage.getItem(tokenName);
+  const { name, id } = await jwtDecode<IToken>(token!);
 
   return { name, id };
 };
