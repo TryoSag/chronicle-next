2ed896bedcb727cd26c8956663d477d14b40fa34
AlbSag <92945087+TryoSag@users.noreply.github.com>
2025-03-14 13:42:35 +0100

    add create and delete chronicle function and refactor types

--- app/actions/chronicle/chronicle.ts
+++ app/actions/chronicle/chronicle.ts
@@ -1,11 +1,5 @@
 import prisma from "@/lib/prisma";
-import { IChronicle } from "@/types/chroniclesTypes";
-
-type correctResponseGetUserChronicles = {
-  status: true;
-  message: string;
-  data: IChronicle[];
-};
+import { IChronicle, ICreateChronicle } from "@/types/chroniclesTypes";
 
 type errorResponse = {
   status: false;
@@ -13,9 +7,19 @@ type errorResponse = {
   data: string;
 };
 
-type responseGetUserChronicles =
-  | correctResponseGetUserChronicles
+type correctResponse = { status: true; message: string };
+
+export type responseGetUserChronicles =
+  | {
+      status: true;
+      message: string;
+      data: IChronicle[];
+    }
   | errorResponse;
+
+export type responseCreateChronicle = correctResponse | errorResponse;
+
+export type responseDeleteChronicle = correctResponse | errorResponse;
 
 export const getUserChrnonicles = async (
   userId: number
@@ -27,6 +31,53 @@ export const getUserChrnonicles = async (
       },
     });
     return { status: true, message: "Your chronicles", data: userChronicles };
+  } catch (e) {
+    return {
+      status: false,
+      message: "Error in the database",
+      data: (e as Error).message,
+    };
+  }
+};
+
+export const createChronicle = async (
+  newChronicle: ICreateChronicle
+): Promise<responseCreateChronicle> => {
+  try {
+    const createdChronicle = await prisma.chronicle.create({
+      data: newChronicle,
+    });
+    if (!createdChronicle) {
+      return {
+        status: false,
+        message: "Error creating the chronicle",
+        data: "",
+      };
+    }
+    return { status: true, message: "Chronicle created" };
+  } catch (e) {
+    return {
+      status: false,
+      message: "Error in the database",
+      data: (e as Error).message,
+    };
+  }
+};
+
+export const deleteChronicle = async (
+  chronicleId: number
+): Promise<responseDeleteChronicle> => {
+  try {
+    await prisma.chronicleTag.deleteMany({
+      where: { chronicleId: chronicleId },
+    });
+
+    await prisma.chronicle.delete({
+      where: {
+        id: chronicleId,
+      },
+    });
+    return { status: true, message: "Chronicle deleted" };
   } catch (e) {
     return {
       status: false,
