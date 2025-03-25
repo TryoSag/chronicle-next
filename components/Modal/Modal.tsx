8a7601a2f9123715b5e01bc58a31498b072664d9
AlbSag <92945087+TryoSag@users.noreply.github.com>
2025-03-25 12:29:49 +0100

    add closeModal function on modal children props

--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -1,16 +1,24 @@
-import { JSX } from "react";
+import { cloneElement, JSX } from "react";
 
 interface propsModal {
   open: boolean;
+  closeModalAction: () => void;
   children: JSX.Element;
 }
 
-const Modal = ({ open, children }: propsModal): JSX.Element => {
+const Modal = ({
+  open,
+  closeModalAction,
+  children,
+}: propsModal): JSX.Element => {
+  const childWithProps = cloneElement(children, {
+    closeModalAction,
+  });
   return (
     <>
       {open && (
         <div className="modal">
-          <div className="modal-content">{children}</div>
+          <div className="modal-content">{childWithProps}</div>
         </div>
       )}
     </>
